5b7fba1011a7643b81b1574663fb5cc21af98ba7
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-06-28 07:30:19 +0900

    fix: コマンド＄の範囲外脱出を対策

--- si.c
+++ si.c
@@ -49,8 +49,7 @@ void showEditor(enum SIMode mode, char line[MAX_LINE_LENGTH], int cursorPos) {
     /* 1行目を描画 */
     printf("%s \n", line);
     /* 2行目を描画 */
-    //if (mode == INSERT) printf("\e[1m-- INSERT --\e[m");    /*太字*/
-    printf("<%d>", cursorPos);
+    if (mode == INSERT) printf("\e[1m-- INSERT --\e[m");    /*太字*/
     /* カーソルを1行上の先頭に移動 */
     printf("\033[1A\r");
     /* cursorPos 回カーソルを進める */
@@ -146,7 +145,7 @@ int editLine(char line[MAX_LINE_LENGTH]) {
                 break;
 
             case '$':   /* カーソルを行末へ */
-                cursorPos = lineLength-1;
+                if (lineLength) cursorPos = lineLength-1;
                 break;
 
             case 'w':   /* カーソルをCSV次列先頭へ */
